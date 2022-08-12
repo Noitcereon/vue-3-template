70f35b0cad914e980860d1fcb5b12c8d60e96c01
Thomas 'Noit' A <tba@live.dk>
2022-08-12 15:17:38 +0200

    Add VITE_BASE_PUBLIC_PATH to TS IntelliSense

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-    readonly VITE_APP_TITLE: string
+interface ImportMetaEnv { // eslint-disable-line
+    readonly VITE_BASE_PUBLIC_PATH: string
     // more env variables...
   }
   
