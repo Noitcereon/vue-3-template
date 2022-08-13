7a8fff9ae03cfccdb057fa1c86835b699e585f4c
Thomas 'Noit' A <tba@live.dk>
2022-08-13 17:41:50 +0200

    Add about page component and route

---
+++ src/pages/about.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import PageHeader from "../components/PageHeader.vue";
+import PageFooter from "../components/PageFooter.vue";
+</script>
+
+<template>
+  <PageHeader />
+
+  <main class="container mx-auto py-20 h-screen">
+    <h1 class="text-3xl text-center">Om mig</h1>
+  </main>
+
+  <PageFooter />
+</template>
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import Index from '@/pages/index.vue';
 import NotFound from '@/pages/not-found.vue';
+import About from '@/pages/about.vue';
 
 export const routes = [
   { path: '/', component: Index },
+  { path: '/om-mig', component: About },
   { path: '/:path(.*)', component: NotFound },
 ];
 
